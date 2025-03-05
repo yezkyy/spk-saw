5577be4138e3cf7712e0d4132dbb8cd377ec2f44
Rizky <muhammadrizkys2004@gmail.com>
2025-03-05 08:41:13 +0800

    Add models and migrations for Alternatif, Kriteria, and NilaiAlternatif

---
+++ database/migrations/2025_03_05_003953_create_nilai_alternatifs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nilai_alternatifs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('alternatif_id')->constrained();
+            $table->foreignId('kriteria_id')->constrained();
+            $table->double('nilai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nilai_alternatifs');
+    }
+};
