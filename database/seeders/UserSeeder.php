2393fcfa4ea8ff47648e915666b9462ee7f18947
Rizky <muhammadrizkys2004@gmail.com>
2025-03-05 08:50:20 +0800

    Refactor database seeder to use UserSeeder for user creation

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'), // Gunakan password yang mudah diingat
+        ]);
+    }
+}
